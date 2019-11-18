7c28751ae06ecf9af143ebbca95166e0dce1ee1f
Edward Greenberg <ed.w.greenberg@gmail.com>
2019-11-18 14:47:07 -0500

    wall

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,9 +39,7 @@ const Layout = ({ children }) => {
           ©
           {' '}
           {new Date().getFullYear()}
-, Built with
-
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
     </>
