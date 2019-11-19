5437ec4e466e8d3085b89050bbedfbb30caee16e
Edward Greenberg <ed.w.greenberg@gmail.com>
2019-11-19 12:53:19 -0500

    testspace

--- src/components/header.js
+++ src/components/header.js
@@ -16,7 +16,7 @@ const Header = ({ siteTitle }) => (
         padding: '1.45rem 1.0875rem',
       }}
     >
-      <h1 style={{ margin: 1 }}>
+      <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
