69ba205ff123344bc256152f5ca75096b838115a
Edward Greenberg <ed.w.greenberg@gmail.com>
2019-11-19 13:17:01 -0500

    realpink

--- src/components/header.js
+++ src/components/header.js
@@ -20,7 +20,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: 'pink',
+            color: '#FF00AB',
             textDecoration: 'none',
           }}
         >
